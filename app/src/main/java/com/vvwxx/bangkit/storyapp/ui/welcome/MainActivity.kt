f90df225eabc882f2a395f55e5cda0b9842ca192
AdnSmile <nessafitri16@gmail.com>
2023-04-13 14:13:35 +0700

    Buat tampilan awal untuk login dan register

---
+++ app/src/main/java/com/vvwxx/bangkit/storyapp/ui/welcome/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.vvwxx.bangkit.storyapp.ui.welcome
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.vvwxx.bangkit.storyapp.R
+import com.vvwxx.bangkit.storyapp.databinding.ActivityMainBinding
+import com.vvwxx.bangkit.storyapp.ui.login.LoginActivity
+import com.vvwxx.bangkit.storyapp.ui.register.RegisterActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.signupButton.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
